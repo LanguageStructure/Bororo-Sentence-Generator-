0758827c9fa3cc0ab43fd1f465fab5f74d54208c
FFGerardi <47945643+LanguageStructure@users.noreply.github.com>
2026-09-22 17:25:04 -0400

    Test reviewed ako person paradigm additions

--- tests/test_compositional.py
+++ tests/test_compositional.py
@@ -4,6 +4,10 @@ def test_reviewed_ako_cells():
  assert realize_ako(person=1,number="Sing",mood="Ind")=="inagore"
  assert realize_ako(person=3,number="Sing",mood="Ind")=="akore"
  assert realize_ako(person=3,number="Plur",mood="Ind")=="egore"
+ assert realize_ako(person=2,number="Sing")=="akago"
+ assert realize_ako(person=2,number="Sing",mood="Ind")=="akagore"
+ assert realize_ako(person=1,number="Plur",clusivity="In")=="pagago"
+ assert realize_ako(person=1,number="Plur",clusivity="In",mood="Ind")=="pagagore"
  assert realize_ako(person=1,number="Sing",mood="Ind",polarity="Neg")=="inagokare"
  assert realize_ako(person=1,number="Plur",clusivity="Ex",mood="Ind")=="cenagore"
  assert realize_ako(person=3,number="Sing",mood="Ind",status="Irr")=="akomode"
@@ -13,5 +17,5 @@ def test_reviewed_nominal_possessive():
  assert realize_ako(person=3,number="Plur",usage="nominal_possessive")=="ego"
 
 def test_unlicensed_cells_are_not_invented():
- assert realize_ako(person=2,number="Sing",mood="Ind") is None
+ assert realize_ako(person=2,number="Plur",mood="Ind") is None
  assert realize_ako(person=3,number="Sing",derivation="Cau") is None
