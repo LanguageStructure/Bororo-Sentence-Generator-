3ad79a76051d4cdc92778cd2b8009dd79f5d946a
FFGerardi <47945643+LanguageStructure@users.noreply.github.com>
2026-09-23 09:00:13 -0400

    Import reviewed construction helper in person-index tests

--- tests/test_person_index.py
+++ tests/test_person_index.py
@@ -1,4 +1,4 @@
-from bororo_generator.person_index import indexed_stem,indexed_reviewed_stem,reviewed_person_cell,reviewed_person_analysis
+from bororo_generator.person_index import indexed_stem,indexed_reviewed_stem,reviewed_person_cell,reviewed_person_analysis,reviewed_construction_cell
 def test_reviewed_stem_classes():
     assert indexed_stem("aregodu","1SG","O")=="itaregodu"
     assert indexed_stem("aregodu","2SG","O")=="akaregodu"
