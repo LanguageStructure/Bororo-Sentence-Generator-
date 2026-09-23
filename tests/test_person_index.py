ee09c33a0de89cded5395136a0e868ba17deccfd
FFGerardi <47945643+LanguageStructure@users.noreply.github.com>
2026-09-23 05:59:49 -0400

    Fix reviewed_person_cell test import

--- tests/test_person_index.py
+++ tests/test_person_index.py
@@ -1,4 +1,4 @@
-from bororo_generator.person_index import indexed_stem,indexed_reviewed_stem
+from bororo_generator.person_index import indexed_stem,indexed_reviewed_stem,reviewed_person_cell
 def test_reviewed_stem_classes():
     assert indexed_stem("aregodu","1SG","O")=="itaregodu"
     assert indexed_stem("aregodu","2SG","O")=="akaregodu"
