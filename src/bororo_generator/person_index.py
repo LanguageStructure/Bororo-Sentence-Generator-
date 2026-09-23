e924393b38dc62475d7a3d3531fc32b2ce1716f1
FFGerardi <47945643+LanguageStructure@users.noreply.github.com>
2026-09-23 09:08:53 -0400

    Add reviewed mako 3PL cell

--- src/bororo_generator/person_index.py
+++ src/bororo_generator/person_index.py
@@ -32,7 +32,7 @@ REVIEWED_PERSON_CELLS={
     "meru":{"2SG":"ameru","1PL.EXCL":"cemeru","3PL":"emeru"},
     "kodu":{"1SG":"ikodu","1PL.EXCL":"cegodu","3PL":"ekodu","CORF":"tugodu"},
     "maragodu":{"2PL":"tamaragodu"},
-    "mako":{"1SG":"imago","2SG":"amago","3SG":"mako","1PL.EXCL":"cemago"},
+    "mako":{"1SG":"imago","2SG":"amago","3SG":"mako","1PL.EXCL":"cemago","3PL":"emago"},
     "kudu":{"2SG":"akudu"},
     "maku":{"3SG":"maku","3PL":"emagu"},
 }
