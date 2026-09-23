1e5bbf6df54859aa2bc97646bf1050a9281ad870
FFGerardi <47945643+LanguageStructure@users.noreply.github.com>
2026-09-23 08:45:51 -0400

    Use IND terminology for Bororo -re

--- tests/test_morphotactics.py
+++ tests/test_morphotactics.py
@@ -1,18 +1,18 @@
 from bororo_generator.morphotactics import ordering_licensed, ordering_violations
 def test_directly_attested_order():
-    assert ordering_licensed(["PERSON","LEX","IRR","NEG","DECL"])
-    assert ordering_licensed(["LEX","ASPECT","DECL"])
+    assert ordering_licensed(["PERSON","LEX","IRR","NEG","IND"])
+    assert ordering_licensed(["LEX","ASPECT","IND"])
     assert ordering_licensed(["LEX","IRR","NEG","QUOT"])
 def test_reject_reversed_attested_relation():
-    assert ("IRR","NEG") in ordering_violations(["LEX","NEG","IRR","DECL"])
+    assert ("IRR","NEG") in ordering_violations(["LEX","NEG","IRR","IND"])
 
 
-def test_reviewed_irrealis_declarative_surface():
-    from bororo_generator.morphotactics import reviewed_operator_surface,realize_irrealis_declarative
-    assert reviewed_operator_surface("IRR","DECL")=="mode"
-    assert realize_irrealis_declarative("ikodu")=="ikodumode"
+def test_reviewed_irrealis_indicative_surface():
+    from bororo_generator.morphotactics import reviewed_operator_surface,realize_irrealis_indicative
+    assert reviewed_operator_surface("IRR","IND")=="mode"
+    assert realize_irrealis_indicative("ikodu")=="ikodumode"
 
 
-def test_reviewed_irrealis_negative_declarative_surface():
+def test_reviewed_irrealis_negative_indicative_surface():
     from bororo_generator.morphotactics import reviewed_operator_surface
-    assert reviewed_operator_surface("IRR","NEG","DECL")=="modukare"
+    assert reviewed_operator_surface("IRR","NEG","IND")=="modukare"
